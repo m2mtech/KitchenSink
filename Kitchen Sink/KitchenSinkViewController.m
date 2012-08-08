3345d948ebf0a3f93fca310d1de968f71e92bdd7
m2m tech <tech@m2m.at>
2012-08-08 19:18:49 +0200

    end of lecture #15

--- Kitchen Sink/KitchenSinkViewController.m
+++ Kitchen Sink/KitchenSinkViewController.m
@@ -7,32 +7,73 @@
 //
 
 #import "KitchenSinkViewController.h"
+#import "AskerViewController.h"
 
-@interface KitchenSinkViewController ()
+@interface KitchenSinkViewController () <AskerViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *kitchenSink;
 
 @end
 
 @implementation KitchenSinkViewController
 
-- (void)viewDidLoad
+@synthesize kitchenSink = _kitchenSink;
+
+- (IBAction)tap:(UITapGestureRecognizer *)gesture 
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    CGPoint tapLocation = [gesture locationInView:self.kitchenSink];
+    
+    for (UIView *view in self.kitchenSink.subviews) {
+        if (CGRectContainsPoint(view.frame, tapLocation)) {
+            [UIView animateWithDuration:4.0 animations:^{
+                [self setRandomLocationForView:view];
+            }];
+        }
+    }
 }
 
-- (void)viewDidUnload
+- (void)setRandomLocationForView:(UIView *)view
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    [view sizeToFit];
+    CGRect sinkBounds = CGRectInset(self.kitchenSink.bounds, 
+                                    view.frame.size.width / 2, 
+                                    view.frame.size.height / 2);
+    CGFloat x = arc4random() % (int)sinkBounds.size.width + view.frame.size.width / 2;
+    CGFloat y = arc4random() % (int)sinkBounds.size.height + view.frame.size.height / 2;
+    view.center = CGPointMake(x, y);    
+}
+
+- (void)addLabel:(NSString *)text
+{
+    UILabel *label = [[UILabel alloc] init];
+    label.text = text;
+    label.font = [UIFont systemFontOfSize:48.0];
+    label.backgroundColor = [UIColor clearColor];
+    [self setRandomLocationForView:label];
+    [self.kitchenSink addSubview:label];
+}
+
+- (void)askerViewController:(AskerViewController *)sender 
+             didAskQuestion:(NSString *)question 
+               andGotAnswer:(NSString *)answer
+{
+    [self addLabel:answer];
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier hasPrefix:@"Create Label"]) {
+        AskerViewController *asker = (AskerViewController *)segue.destinationViewController;
+        asker.answer = @"Label Text";
+        asker.question = @"What do you want your label to say?";
+        asker.delegate = self;        
+    }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    return YES;
 }
 
 @end
